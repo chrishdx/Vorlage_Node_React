eda8ad45d8b22e1686a736331fdefb64feeec3b1
chrishdx <50511550+chrishdx@users.noreply.github.com>
2023-02-05 10:07:10 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,6 +3,9 @@
 rm -rf .git
 npx create-react-app frontend --template cra-template-pwa
 copy ./Templates/frontend/* ./frontend/
+cd frontend
+npm install
+cd ..
 mkdir backend
 cd backend
 npm init -y
