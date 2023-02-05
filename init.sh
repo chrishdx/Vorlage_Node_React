fa465e23902c096fbc27520fd27dec051023a736
chrishdx <50511550+chrishdx@users.noreply.github.com>
2023-02-05 10:09:56 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,13 +3,10 @@
 rm -rf .git
 npx create-react-app frontend --template cra-template-pwa
 copy ./Templates/frontend/* ./frontend/
-cd frontend
-npm install
-cd ..
+npm install --prefix ./frontend
 mkdir backend
-cd backend
-npm init -y
-npm i express body-parser bcrypt better-sqlite3 dotenv express-session moment-timezone uuidv4
+npm init -y --prefix ./backend
+npm i  --prefix ./backend express body-parser bcrypt better-sqlite3 dotenv express-session moment-timezone uuidv4
 copy ./Templates/backend/* ./backend/
-cd ..
+
 
