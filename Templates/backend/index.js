e96c2b6cfb989f1783c07c2b6fb23db033f6b3da
chrishdx <50511550+chrishdx@users.noreply.github.com>
2023-02-05 10:05:03 +0100

    Update index.js

--- Templates/backend/index.js
+++ Templates/backend/index.js
@@ -3,3 +3,9 @@ echo "const app = require('express')();
 require('dotenv').config();
 const NODE_ENV = process.env.NODE_ENV || 'dev';
 const Port = NODE_ENV === "production" ? parseInt(process.env.Port) || 507 : 2210" 
+
+
+
+app.listen(Port , () => {
+console.log(`Server läuft auf Port ${Port}`
+}
