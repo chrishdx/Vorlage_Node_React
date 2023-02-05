4ca98e4c27e1d1af2981a8ba28135923c64ce663
chrishdx <50511550+chrishdx@users.noreply.github.com>
2023-02-05 10:14:42 +0100

    Update index.js

--- Templates/backend/index.js
+++ Templates/backend/index.js
@@ -2,7 +2,7 @@
 const app = require('express')();
 require('dotenv').config();
 const NODE_ENV = process.env.NODE_ENV || 'dev';
-const Port = NODE_ENV === "production" ? parseInt(process.env.Port) || 507 : 2210" 
+const Port = NODE_ENV === "production" ? parseInt(process.env.Port) || 507 : 2210
 
 
 
