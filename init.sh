20e0c2848e6138681fb44e42af4b7e2a30b79fa7
chrishdx <50511550+chrishdx@users.noreply.github.com>
2023-08-06 09:48:13 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 rm -rf .git
 npx create-react-app frontend --template cra-template-pwa
-cp ./Templates/frontend/* ./frontend/
-npm install --prefix ./frontend
+cp $DIR/Templates/frontend/* frontend/
+npm install --prefix frontend
 mkdir backend
-npm init -y --prefix ./backend
-npm i  --prefix ./backend express body-parser bcrypt better-sqlite3 dotenv express-session moment-timezone uuidv4
-cp ./Templates/backend/* ./backend/
+npm init -y --prefix backend
+npm i  --prefix backend express body-parser bcrypt better-sqlite3 dotenv express-session moment-timezone uuidv4
+cp $DIR/Templates/backend/* backend/
+
 
 
